c42c9202451fc8f1ef631e39dc2fc0873c838adc
Dreamwalker <aristojeff@gmail.com>
2018-11-13 01:16:51 +0900

    icon added and update model

--- app/src/main/java/com/dreamwalkers/elab_yang/mmk/model/drugselect/TrackList.java
+++ app/src/main/java/com/dreamwalkers/elab_yang/mmk/model/drugselect/TrackList.java
@@ -11,15 +11,27 @@ import java.util.List;
  */
 public class TrackList {
     public final static List<Track> TRACKS = new ArrayList<Track>() {{
-
-        add(new Track("휴머로그", "초속효성", R.drawable.ic_syringe_rrpid));
+        add(new Track("노보믹스30", "혼합형", R.drawable.ic_syringe_mix));
+        add(new Track("노보믹스50", "혼합형", R.drawable.ic_syringe_long));
+        add(new Track("노보믹스70", "혼합형", R.drawable.ic_syringe_long));
+        add(new Track("믹스타드30", "혼합형", R.drawable.ic_syringe_long));
+        add(new Track("휴마로그믹스25", "혼합형", R.drawable.ic_syringe_long));
+        add(new Track("휴마로그믹스50", "혼합형", R.drawable.ic_syringe_long));
+        add(new Track("휴물린70/30", "혼합형", R.drawable.ic_syringe_long));
         add(new Track("노보래피드", "초속효성", R.drawable.ic_syringe_rrpid));
+        add(new Track("휴마로그", "초속효성", R.drawable.ic_syringe_rrpid));
         add(new Track("애피드라", "초속효성", R.drawable.ic_syringe_rrpid));
-        add(new Track("휴먼인슐린", "속효성", R.drawable.ic_syringe_mix));
-        add(new Track("노보린", "중간형", R.drawable.ic_syringe_rrpid));
-        add(new Track("노보렛", "중간형", R.drawable.ic_syringe_mix));
-        add(new Track("휴불린", "중간형", R.drawable.ic_syringe_rrpid));
-        add(new Track("인슈라타드", "중간형", R.drawable.ic_syringe_mix));
+        add(new Track("휴물린R", "속효성", R.drawable.ic_syringe_rapid));
+        add(new Track("노보린R", "속효성", R.drawable.ic_syringe_rapid));
+        add(new Track("노보렛R", "속효성", R.drawable.ic_syringe_rapid));
+        add(new Track("인슈라타드", "중간형", R.drawable.ic_syringe_middle));
+        add(new Track("휴물린N", "중간형", R.drawable.ic_syringe_middle));
+        add(new Track("노보린N", "중간형", R.drawable.ic_syringe_middle));
+        add(new Track("노보렛N", "중간형", R.drawable.ic_syringe_middle));
+        add(new Track("란투스", "지속형", R.drawable.ic_syringe_long));
+        add(new Track("레버미어", "지속형", R.drawable.ic_syringe_long));
+        add(new Track("투제오", "지속형", R.drawable.ic_syringe_long));
+        add(new Track("트레시바", "지속형", R.drawable.ic_syringe_long));
 
     }};
 }
