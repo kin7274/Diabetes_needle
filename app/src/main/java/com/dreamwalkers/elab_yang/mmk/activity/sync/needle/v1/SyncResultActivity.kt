54915ab5140bf3aaaefe87ea6f3a33a93416260c
Dreamwalker <aristojeff@gmail.com>
2018-11-12 21:45:37 +0900

    received json result in text view

--- app/src/main/java/com/dreamwalkers/elab_yang/mmk/activity/sync/needle/v1/SyncResultActivity.kt
+++ app/src/main/java/com/dreamwalkers/elab_yang/mmk/activity/sync/needle/v1/SyncResultActivity.kt
@@ -3,6 +3,7 @@ package com.dreamwalkers.elab_yang.mmk.activity.sync.needle.v1
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.dreamwalkers.elab_yang.mmk.R
+import kotlinx.android.synthetic.main.activity_sync_result.*
 import org.jetbrains.anko.toast
 
 class SyncResultActivity : AppCompatActivity() {
@@ -11,7 +12,9 @@ class SyncResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sync_result)
 
-        val values = intent.getStringExtra("SyncValue");
+        val values = intent.getStringExtra("SyncValue")
         toast(values)
+
+        result_text.text = values
     }
 }
