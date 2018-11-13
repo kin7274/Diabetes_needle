8aaab69462daa40278e595ce71b1c289c896cb3f
Dreamwalker <aristojeff@gmail.com>
2018-11-13 14:59:25 +0900

    change drawer menu

--- app/src/main/java/com/dreamwalkers/elab_yang/mmk/activity/HomeActivity.java
+++ app/src/main/java/com/dreamwalkers/elab_yang/mmk/activity/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.dreamwalkers.elab_yang.mmk.activity;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -8,23 +9,44 @@ import android.support.annotation.ColorInt;
 import android.support.annotation.ColorRes;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.dreamwalker.slidedrawer.SlidingRootNav;
 import com.dreamwalker.slidedrawer.SlidingRootNavBuilder;
 import com.dreamwalkers.elab_yang.mmk.R;
+import com.dreamwalkers.elab_yang.mmk.activity.fragment.FragmentA;
+import com.dreamwalkers.elab_yang.mmk.activity.fragment.FragmentB;
+import com.dreamwalkers.elab_yang.mmk.activity.fragment.FragmentC;
+import com.dreamwalkers.elab_yang.mmk.activity.fragment.FragmentD;
+import com.dreamwalkers.elab_yang.mmk.activity.select.SelectDrugActivity;
 import com.dreamwalkers.elab_yang.mmk.menu.DrawerAdapter;
 import com.dreamwalkers.elab_yang.mmk.menu.DrawerItem;
 import com.dreamwalkers.elab_yang.mmk.menu.SimpleItem;
 import com.dreamwalkers.elab_yang.mmk.menu.SpaceItem;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import eu.long1.spacetablayout.SpaceTabLayout;
+import io.paperdb.Paper;
 
 public class HomeActivity extends AppCompatActivity implements DrawerAdapter.OnItemSelectedListener {
+
+
+    @BindView(R.id.toolbar)
+    Toolbar myToolbar;
+    @BindView(R.id.spaceTabLayout)
+    SpaceTabLayout tabLayout;
 
     private static final String TAG = "HomeActivity";
 
@@ -42,14 +64,14 @@ public class HomeActivity extends AppCompatActivity implements DrawerAdapter.OnI
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_home);
+        initSetting();
 
-        Toolbar toolbar = findViewById(R.id.my_toolbar);
-        setSupportActionBar(toolbar);
+
+        setSupportActionBar(myToolbar);
 
         slidingRootNav = new SlidingRootNavBuilder(this)
-                .withToolbarMenuToggle(toolbar)
+                .withToolbarMenuToggle(myToolbar)
                 .withMenuOpened(false)
                 .withContentClickableWhenMenuOpened(false)
                 .withSavedState(savedInstanceState)
@@ -74,6 +96,14 @@ public class HomeActivity extends AppCompatActivity implements DrawerAdapter.OnI
         list.setAdapter(adapter);
 
         adapter.setSelected(POS_DASHBOARD);
+    }
+
+    private void initSetting(){
+        Paper.init(this);
+        ButterKnife.bind(this);
+        setStatusbar();
+        setTapLayout();
+
     }
 
     @Override
@@ -121,6 +151,58 @@ public class HomeActivity extends AppCompatActivity implements DrawerAdapter.OnI
     @ColorInt
     private int color(@ColorRes int res) {
         return ContextCompat.getColor(this, res);
+    }
+
+    public void setStatusbar() {
+        View view = getWindow().getDecorView();
+        view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setStatusBarColor(getResources().getColor(R.color.background));
+    }
+
+    private void setTapLayout() {
+
+        //add the fragments you want to display in a List == 내가 원하면 추가가능하다~ 이말이야
+        List<android.support.v4.app.Fragment> fragmentList = new ArrayList<>();
+        // 홈 = 기록보기
+        fragmentList.add(new FragmentA());
+        // 캘린더
+        fragmentList.add(new FragmentB());
+        // 장치 관리 + 동기화
+        fragmentList.add(new FragmentC());
+        // 프로필
+        fragmentList.add(new FragmentD());
+
+        // set
+//        final CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        tabLayout.initialize(viewPager, getSupportFragmentManager(), fragmentList, null);
+
+        tabLayout.setTabOneOnClickListener(v -> {
+//            Snackbar.make(coordinatorLayout, "Welcome to SpaceTabLayout", Snackbar.LENGTH_SHORT).show();
+        });
+        tabLayout.setOnClickListener(v -> {
+            Toast.makeText(this, "" + tabLayout.getCurrentPosition(), Toast.LENGTH_SHORT).show();
+//            tabLayout.setTabOneOnClickListener(view -> Snackbar.make(coordinatorLayout, "안녕", Snackbar.LENGTH_SHORT).show());
+            tabLayout.setOnClickListener(view -> {
+                Toast.makeText(getApplication(), "" + tabLayout.getCurrentPosition(), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "set: tabLayout.getCurrentPosition() = " + tabLayout.getCurrentPosition());
+                if (tabLayout.getCurrentPosition() == 1) {
+                    Log.d(TAG, "set: 여기!!");
+                    Intent intent = new Intent(this, SelectDrugActivity.class);
+                    startActivity(intent);
+                    intent.putExtra("SelectDrugActivity", false);
+                }
+
+                if (tabLayout.getCurrentPosition() == 2) {
+                    Log.d(TAG, "set: 여기!!");
+                    Intent intent = new Intent(this, NeedleScanActivity.class);
+                    startActivity(intent);
+                    intent.putExtra("flag_toFragment", false);
+                }
+            });
+        });
     }
 
   
