cea5ba2540f067d7248e296b8657ce689c695697
kin7274 <42366332+kin7274@users.noreply.github.com>
2018-11-19 20:45:09 +0900

    필요없는 액티비티(PLAN B) 삭제
    필요없는 액티비티(PLAN B) 삭제

--- app/src/main/java/com/dreamwalkers/elab_yang/mmk/activity/HomeActivity.java
+++ app/src/main/java/com/dreamwalkers/elab_yang/mmk/activity/HomeActivity.java
@@ -1,6 +1,5 @@
 package com.dreamwalkers.elab_yang.mmk.activity;
 
-import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Intent;
 import android.content.res.TypedArray;
@@ -27,13 +26,10 @@ import com.dreamwalkers.elab_yang.mmk.activity.fragment.FragmentA;
 import com.dreamwalkers.elab_yang.mmk.activity.fragment.FragmentB;
 import com.dreamwalkers.elab_yang.mmk.activity.fragment.FragmentC;
 import com.dreamwalkers.elab_yang.mmk.activity.fragment.FragmentD;
-import com.dreamwalkers.elab_yang.mmk.activity.sync.needle.v1.SelectDrugFirstActivity2;
 import com.dreamwalkers.elab_yang.mmk.menu.DrawerAdapter;
 import com.dreamwalkers.elab_yang.mmk.menu.DrawerItem;
 import com.dreamwalkers.elab_yang.mmk.menu.SimpleItem;
 import com.dreamwalkers.elab_yang.mmk.menu.SpaceItem;
-
-import org.jetbrains.anko.AlertDialogBuilder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
