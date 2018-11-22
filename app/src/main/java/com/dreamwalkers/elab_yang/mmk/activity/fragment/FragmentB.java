4aeb15806c5d25ef65e133758a405d90c4b355c2
kin7274 <kms6742@naver.com>
2018-11-23 06:22:28 +0900

    달력 추가
    달력 추가

--- app/src/main/java/com/dreamwalkers/elab_yang/mmk/activity/fragment/FragmentB.java
+++ app/src/main/java/com/dreamwalkers/elab_yang/mmk/activity/fragment/FragmentB.java
@@ -1,27 +1,76 @@
 package com.dreamwalkers.elab_yang.mmk.activity.fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
+import android.widget.TextView;
 
 import com.dreamwalkers.elab_yang.mmk.R;
+import com.shrikanthravi.collapsiblecalendarview.data.Day;
+import com.shrikanthravi.collapsiblecalendarview.widget.CollapsibleCalendar;
+
+import java.time.LocalDate;
+
+import static android.support.constraint.Constraints.TAG;
 
 public class FragmentB extends Fragment {
 
-    private CalendarView calendar_view;
+//    private CalendarView calendar_view;
+
+    private CollapsibleCalendar calendarView;
+    private TextView textview;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_b, container, false);
         //        ButterKnife.bind(this, view);
-        calendar_view = (CalendarView) view.findViewById(R.id.calendar_view);
-
+//        calendar_view = (CalendarView) view.findViewById(R.id.calendar_view);
 //        calendar_view.getDate();
+
+        calendarView = (CollapsibleCalendar) view.findViewById(R.id.calendarView);
+        textview = (TextView) view.findViewById(R.id.textview);
+
+
+        calendarView.setCalendarListener(new CollapsibleCalendar.CalendarListener() {
+            @Override
+            public void onDaySelect() {
+                // 날짜 선택 시
+                Day day = calendarView.getSelectedDay();
+                Log.d(TAG, "onDaySelect: 날짜 선택 = "
+                        + day.getYear() + "/" + (day.getMonth() + 1) + "/" + day.getDay());
+
+                textview.setText((day.getYear() + "/" + (day.getMonth() + 1) + "/" + day.getDay()).toString());
+            }
+
+            @Override
+            public void onItemClick(View view) {
+
+            }
+
+            @Override
+            public void onDataUpdate() {
+
+            }
+
+            @Override
+            public void onMonthChange() {
+
+            }
+
+            @Override
+            public void onWeekChange(int i) {
+
+            }
+        });
+
         return view;
     }
 }
